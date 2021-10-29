9aebd96185879b9b197e66cf176068283f8a35a2
krisrod10 <82917336+krisrod10@users.noreply.github.com>
2021-10-29 11:23:59 -0500

    is it working

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ let mysql = require("./db/db");
 
 
 
+app.get("/", function(req, res){
+    console.log('this app is working');
+    res.json("Is my app working?")
+})
+
 let port = process.env.PORT
 app.listen(port, () => {
     console.log("Listening on port", port);
